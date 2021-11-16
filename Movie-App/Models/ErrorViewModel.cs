79eee6575faebf07d728530d99f87709d6e4e6eb
josephortiz <ortizjoseph7@gmail.com>
2021-11-16 10:34:31 -0600

    first commit

---
+++ Movie-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Movie_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
