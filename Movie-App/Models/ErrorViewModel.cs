7cad8330bbaab28d9a0ee55a6e4205790209a384
josephortiz <ortizjoseph7@gmail.com>
2021-11-16 10:38:00 -0600

    commit

--- Movie-App/Models/ErrorViewModel.cs
+++ Movie-App/Models/ErrorViewModel.cs
@@ -4,8 +4,7 @@ namespace Movie_App.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
-
+        public string RequestId { get; set; 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
